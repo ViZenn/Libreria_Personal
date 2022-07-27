85308d58236222a67d9be6dcf1df9d10e73e3aff
ViZenn <86311653+ViZenn@users.noreply.github.com>
2022-07-27 17:47:08 -0400

    Funciones para procesar BDs con graficos

--- Contagio, Proyeccion y Evolucion/Contagios Semanales/AUX_contagios.py
+++ Contagio, Proyeccion y Evolucion/Contagios Semanales/AUX_contagios.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from matplotlib.ticker import FuncFormatter
+import matplotlib.pyplot as plt
 
 class AUX_Contagios:
     def __init__(self):
@@ -62,4 +64,73 @@ class AUX_Contagios:
                         axis=1)
         print('Creada BD de Chile!')
         return Chile
+    
+    def grafica_casos(self,Valpo,Chile):
+        print('Creando gráficos de barras y lineas para casos nuevos')
+        #aqui se hace el df extra para graficar
+        DF2=Valpo[['Prom_Sem_Valp_Casos','Velocidad_Valp_Casos']].join(Chile[['Prom_Sem_Nac_Casos','Velocidad_Nac_Casos']])
+        DF2['Prom_Sem_Valp_Casos'] = [round(d,2) for d in DF2['Prom_Sem_Valp_Casos']]
+        DF2['Prom_Sem_Nac_Casos'] = [round(d,2) for d in DF2['Prom_Sem_Nac_Casos']]
+        DF2['Velocidad_Nac_Casos'] = [round(d,2) for d in DF2['Velocidad_Nac_Casos']]
+        DF2['Velocidad_Valp_Casos'] = [round(d,2) for d in DF2['Velocidad_Valp_Casos']]
+        DF2=DF2.reset_index()
+        DF2['dia'] = [d.date() for d in DF2['Fecha']]
+        DF2.set_index('dia',inplace=True)
+        #aca se grafica y da forma
+        ax=DF2.plot(y=['Prom_Sem_Valp_Casos', 'Prom_Sem_Nac_Casos'], kind="bar",grid=True,
+                    figsize=(9,8),label=['Casos Valparaíso','Casos Nacionales'])
+        ax2 = ax.twinx()
+        ax2.plot(DF2['Velocidad_Valp_Casos'].values, linestyle='-', linewidth=2.0,color='gray',label='Velocidad Valparaíso')
+        ax2.plot(DF2['Velocidad_Nac_Casos'].values, linestyle='-', linewidth=2.0,color='yellow',label='Velocidad Nacional')
+        ax.set_title("Casos Nuevos", fontsize=25)
+        ax2.yaxis.set_major_formatter(FuncFormatter(lambda y, _: '{:.0%}'.format(y))) 
+        plt.legend(loc=2)
+        plt.savefig("Barra_Linea_Casos_Nuevos.jpg")
+        print('Gráfico Exportado!')
+    
+    def grafica_uci(self,Valpo,Chile):
+        print('Creando gráficos de barras y lineas para UCI')
+        #aqui se hace el df extra para graficar
+        DF2=Valpo[['Prom_Sem_Valp_UCI','Velocidad_Valp_UCI']].join(Chile[['Prom_Sem_Nac_UCI','Velocidad_Nac_UCI']])
+        DF2['Prom_Sem_Valp_UCI'] = [round(d,2) for d in DF2['Prom_Sem_Valp_UCI']]
+        DF2['Prom_Sem_Nac_UCI'] = [round(d,2) for d in DF2['Prom_Sem_Nac_UCI']]
+        DF2['Velocidad_Nac_UCI'] = [round(d,2) for d in DF2['Velocidad_Nac_UCI']]
+        DF2['Velocidad_Valp_UCI'] = [round(d,2) for d in DF2['Velocidad_Valp_UCI']]
+        DF2=DF2.reset_index()
+        DF2['dia'] = [d.date() for d in DF2['Fecha']]
+        DF2.set_index('dia',inplace=True)
+        #aca se grafica y da forma
+        ax=DF2.plot(y=['Prom_Sem_Valp_UCI', 'Prom_Sem_Nac_UCI'], kind="bar",grid=True,
+                    figsize=(9,8),label=['UCI Valparaíso','UCI Nacional'])
+        ax2 = ax.twinx()
+        ax2.plot(DF2['Velocidad_Valp_UCI'].values, linestyle='-', linewidth=2.0,color='gray',label='Velocidad Valparaíso')
+        ax2.plot(DF2['Velocidad_Nac_UCI'].values, linestyle='-', linewidth=2.0,color='yellow',label='Velocidad Nacional')
+        ax.set_title("UCI", fontsize=25)
+        ax2.yaxis.set_major_formatter(FuncFormatter(lambda y, _: '{:.0%}'.format(y))) 
+        plt.legend(loc=1)
+        plt.savefig("Barra_Linea_UCI.jpg")
+        print('Gráfico Exportado!')
+        
+    def grafica_deads(self,Valpo,Chile):
+        print('Creando gráficos de barras y lineas para Fallecidos')
+        #aqui se hace el df extra para graficar
+        DF2=Valpo[['Prom_Sem_Valp_Muertes','Velocidad_Valp_Muertes']].join(Chile[['Prom_Sem_Nac_Muertes','Velocidad_Nac_Muertes']])
+        DF2['Prom_Sem_Valp_Muertes'] = [round(d,2) for d in DF2['Prom_Sem_Valp_Muertes']]
+        DF2['Prom_Sem_Nac_Muertes'] = [round(d,2) for d in DF2['Prom_Sem_Nac_Muertes']]
+        DF2['Velocidad_Nac_Muertes'] = [round(d,2) for d in DF2['Velocidad_Nac_Muertes']]
+        DF2['Velocidad_Valp_Muertes'] = [round(d,2) for d in DF2['Velocidad_Valp_Muertes']]
+        DF2=DF2.reset_index()
+        DF2['dia'] = [d.date() for d in DF2['Fecha']]
+        DF2.set_index('dia',inplace=True)
+        #aca se grafica y da forma
+        ax=DF2.plot(y=['Prom_Sem_Valp_Muertes', 'Prom_Sem_Nac_Muertes'], kind="bar",grid=True,
+                    figsize=(9,8),label=['Fallecimientos Valparaíso','Fallecimientos Nacional'])
+        ax2 = ax.twinx()
+        ax2.plot(DF2['Velocidad_Valp_Muertes'].values, linestyle='-', linewidth=2.0,color='gray',label='Velocidad Valparaíso')
+        ax2.plot(DF2['Velocidad_Nac_Muertes'].values, linestyle='-', linewidth=2.0,color='yellow',label='Velocidad Nacional')
+        ax.set_title("Fallecimientos", fontsize=25)
+        ax2.yaxis.set_major_formatter(FuncFormatter(lambda y, _: '{:.0%}'.format(y))) 
+        plt.legend(loc=1)
+        plt.savefig("Barra_Linea_Deads.jpg")
+        print('Gráfico Exportado!')
 
